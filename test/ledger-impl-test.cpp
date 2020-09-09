da69cf2282c549dab0b80ba1ed1b2517cfb2e77e
tylerliu <tylersiqi@163.com>
2020-09-08 22:15:02 -0700

    config test program for ndncert exp

--- test/ledger-impl-test.cpp
+++ test/ledger-impl-test.cpp
@@ -17,32 +17,32 @@ std::list<std::string> startingPeerPath({
                                                 "./test-certs/test-d.cert"
                                         });
 
-std::shared_ptr<ndn::Data>
-makeData(const std::string& name, const std::string& content)
-{
-  using namespace ndn;
-  using namespace std;
-  auto data = make_shared<Data>(ndn::Name(name));
-  data->setContent((const uint8_t*)content.c_str(), content.size());
-  ndn::SignatureSha256WithRsa fakeSignature;
-  fakeSignature.setValue(ndn::encoding::makeEmptyBlock(tlv::SignatureValue));
-  data->setSignature(fakeSignature);
-  data->wireEncode();
-  return data;
-}
-
 void periodicAddRecord(shared_ptr<Ledger> ledger, Scheduler& scheduler) {
-    Record record(RecordType::GENERIC_RECORD, std::to_string(std::rand()));
-    record.addRecordItem(makeStringBlock(255, std::to_string(std::rand())));
-    record.addRecordItem(makeStringBlock(255, std::to_string(std::rand())));
-    record.addRecordItem(makeStringBlock(255, std::to_string(std::rand())));
+    Record record(RecordType::GENERIC_RECORD, std::string("Test_") + std::to_string(std::rand()));
+    security::KeyChain keychain;
+    const auto& pib = keychain.getPib();
+    const auto& identity = pib.getIdentity("/example");
+    const auto& key = identity.getDefaultKey();
+    security::v2::Certificate newCert;
+
+    Name certName = key.getName();
+    certName.append("test-dledger").append(std::to_string(std::rand()));
+    newCert.setName(certName);
+    newCert.setContent(key.getDefaultCertificate().getContent());
+    SignatureInfo signatureInfo;
+    signatureInfo.setValidityPeriod(security::ValidityPeriod(time::system_clock::now(), time::system_clock::now() + time::days(1)));
+    security::SigningInfo signingInfo(security::SigningInfo::SIGNER_TYPE_ID,
+                                      "/example", signatureInfo);
+    newCert.setFreshnessPeriod(time::days(1));
+    keychain.sign(newCert, signingInfo);
+    record.addRecordItem(newCert.wireEncode());
     ReturnCode result = ledger->createRecord(record);
     if (!result.success()) {
         std::cout << "- Adding record error : " << result.what() << std::endl;
     }
 
     // schedule for the next record generation
-    scheduler.schedule(time::seconds(10), [ledger, &scheduler] { periodicAddRecord(ledger, scheduler); });
+    scheduler.schedule(time::seconds(5), [ledger, &scheduler] { periodicAddRecord(ledger, scheduler); });
 }
 
 int
@@ -58,7 +58,7 @@ main(int argc, char** argv)
   security::KeyChain keychain;
   std::shared_ptr<Config> config = nullptr;
   try {
-    config = Config::CustomizedConfig("/dledger-multicast", "/dledger/" + idName,
+    config = Config::CustomizedConfig("/ndn/multicast/CA-dledger", "/ndn/dledger/" + idName,
             std::string("./dledger-anchor.cert"), std::string("/tmp/dledger-db/" + idName),
                                       startingPeerPath);
     mkdir("/tmp/dledger-db/", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
@@ -69,6 +69,36 @@ main(int argc, char** argv)
   }
 
   shared_ptr<Ledger> ledger = std::move(Ledger::initLedger(*config, keychain, face));
+  ledger->setOnRecordAppCheck([&](const Data& r) {
+      Record record = r;
+      if (record.getType() == GENERIC_RECORD) {
+          bool hasName = false;
+          bool hasCert = false;
+          for (const auto &item : record.getRecordItems()) {
+              if (item.type() == tlv::Name) {
+                  if (hasName) return false;
+                  if (security::v2::Certificate::isValidName(Name(item))) {
+                      hasName = true;
+                  } else {
+                      return false;
+                  }
+              } else if (item.type() == tlv::Data) {
+                  if (hasCert) return false;
+                  try {
+                      auto c = security::v2::Certificate(item);
+                      hasCert = true;
+                  } catch (std::exception &e) {
+                      return false;
+                  }
+              } else {
+                  return false;
+              }
+          }
+          if (hasCert && hasName) return false;
+          if (!hasCert && !hasName) return false;
+      }
+      return true;
+  });
 
   Scheduler scheduler(ioService);
   periodicAddRecord(ledger, scheduler);
