8ac424251fc847f9ef2360d7a40105c6bce803d5
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2020-10-27 21:38:05 -0700

    update

--- dfi-app/dynamic-function-runner.h
+++ dfi-app/dynamic-function-runner.h
@@ -6,37 +6,62 @@
 #define DLEDGER_DYNAMIC_FUNCTION_RUNNER_H
 
 #include <wasm.h>
-#include <ndn-cxx/encoding/block.hpp>
 #include <wasmtime.h>
+
+#include <ndn-cxx/encoding/block.hpp>
 
 /**
  * Because of C integration issue, there should be only one of this object running
  * at any given time.
  */
-class DynamicFunctionRunner{
+class DynamicFunctionRunner {
 public:
-    DynamicFunctionRunner();
-    ~DynamicFunctionRunner();
+  DynamicFunctionRunner();
+  ~DynamicFunctionRunner();
 
-    void run_wasm_block(const ndn::Block& block);
+  void
+  runWatFile(const std::string &fileName);
 
-    void setGetBlockFunc(std::function<ndn::optional<ndn::Block>(ndn::Block)> getBlock);
-    void setSetBlockFunc(std::function<int(ndn::Block, ndn::Block)> setBlock);
+  void
+  runWasmFile(const std::string &fileName);
+
+  void
+  runWasmBlock(const ndn::Block &block);
+
+  void
+  setGetBlockFunc(std::function<ndn::optional<ndn::Block>(ndn::Block)> getBlock);
+
+  void
+  setSetBlockFunc(std::function<int(ndn::Block, ndn::Block)> setBlock);
 
 private:
-    wasm_module_t *compile(const ndn::Block& block);
-    wasm_instance_t *instantiate(wasm_module_t *module, const wasm_extern_t **imports, size_t import_length);
-    wasmtime_linker_t *instantiate_wasi(wasm_module_t *module);
-    void run_program(wasmtime_linker_t *linker_program);
-    void exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap);
+  void
+  runWasmModule(wasm_module_t *module);
 
-    int getBlockCallback(int len, wasm_memory_t *memory);
-    int setBlockCallback(int len_param, int len_value, wasm_memory_t *memory);
+  wasm_module_t *
+  compile(wasm_byte_vec_t *wasm);
 
-    std::function<ndn::optional<ndn::Block>(ndn::Block)> m_getBlock;
-    std::function<int(ndn::Block, ndn::Block)> m_setBlock;
-    wasm_engine_t *m_engine;
-    wasm_store_t *m_store;
+  wasm_instance_t *
+  instantiate(wasm_module_t *module, const wasm_extern_t **imports, size_t import_length);
+
+  wasmtime_linker_t *
+  instantiate_wasi(wasm_module_t *module);
+
+  void
+  run_program(wasmtime_linker_t *linker_program);
+
+  void
+  exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap);
+
+  int
+  getBlockCallback(int len, wasm_memory_t *memory);
+  int
+  setBlockCallback(int len_param, int len_value, wasm_memory_t *memory);
+
+  std::function<ndn::optional<ndn::Block>(ndn::Block)> m_getBlock;
+  std::function<int(ndn::Block, ndn::Block)> m_setBlock;
+  wasm_engine_t *m_engine;
+  wasm_store_t *m_store;
 };
 
-#endif //DLEDGER_DYNAMIC_FUNCTION_RUNNER_H
+#endif  //DLEDGER_DYNAMIC_FUNCTION_RUNNER_H
